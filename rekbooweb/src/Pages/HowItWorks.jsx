1377eebfe5fff52048c0605008964c79c3c5742e
Ilhan Neziric <81989212+ilhanneziric@users.noreply.github.com>
2023-04-03 23:42:26 +0200

    Added how it works with responsiveness

--- rekbooweb/src/Pages/HowItWorks.jsx
+++ rekbooweb/src/Pages/HowItWorks.jsx
@@ -1,8 +1,10 @@
+import '../Styles/howitworks.scss'
 import Footer from "../Components/Footer"
 import Header from "../Components/Header"
 import { updStep } from '../redux/actions/stepActions';
 import { useDispatch } from 'react-redux';
 import { useEffect } from "react";
+import Stepper from '../Assets/stepbystep.png'
 
 const HowItWorks = () => {
   const dispatch = useDispatch();
@@ -13,7 +15,28 @@ const HowItWorks = () => {
   return (
     <>
       <Header/>
-      <Footer/>
+      <div className="howItWorksContainer">
+        <div className="stepperContainer">
+            <img className="stepper" src={Stepper} alt="step by step" />
+        </div>
+
+        <div className="howItWorksItemContainer">
+          <div className="howItWorksItem">
+            <b>1. Prilagodi svoj plan ishrane</b><br /> Uživaj u savršeno prilagodjenom planu ishrane za sebe i svoje kućanstvo, bez ograničenja! Fleksibilan je i prilagođava se tvom 
+            životnom stilu. Želiš promijeniti plan ili otkazati? Nema problema.
+          </div>
+          <div className="howItWorksItem">
+            <b>2. Izaberi obroke</b><br /> Izaberi omiljena jela ili probaj nove kulture. Tradicionali bosanske, talijanske, azijske kuhinje i drugih samo su dio našeg bogatog asortimana.
+          </div>
+          <div className="howItWorksItem howItWorksItemBreak">
+            <b>3. Dostavljamo na tvoja vrata</b><br /> Klikni "naruči" i mi dostavljamo ukupan set obroka sa svježim namirnicama za spremanje tvojih obroka.
+          </div>
+          <div className="howItWorksItem">
+            <b>4. Kuhaj i uživaj</b><br /> Okusi čari kuhanja kod kuće uz pomoć naših detaljnih recepata koji dolaze uz svaki set. Prijatno!
+          </div>
+        </div>
+      </div>
+      {/* <Footer/> */}
     </>  
   )
 }
