df419b51727b86f8e0a353800b8eb31d9a6befc9
Ilhan Neziric <81989212+ilhanneziric@users.noreply.github.com>
2023-04-27 21:00:12 +0200

    Edited navigation items bugfix

--- rekbooweb/src/Components/Navigation.jsx
+++ rekbooweb/src/Components/Navigation.jsx
@@ -13,8 +13,8 @@ const Navigation = () => {
     window.location.pathname !== '/adminreports') ?
     <div className="navigationContainer">
         {/* <Link to='/aboutus'><div className="navigationItem">O NAMA</div></Link> */}
-        <Link to='/howitworks'><div className="navigationItem">KAKO RADI REKBOO</div></Link>
-        <Link to='/plannerplan'><div className="navigationItem">PROBAJ REKBOO</div></Link>
+        <span className="navigationItemContainer"><Link to='/howitworks'><p className="navigationItem">KAKO RADI REKBOO</p></Link></span>
+        <span className="navigationItemContainer"><Link to='/plannerplan'><p className="navigationItem">PROBAJ REKBOO</p></Link></span>
     </div>:
     <></>
   }
