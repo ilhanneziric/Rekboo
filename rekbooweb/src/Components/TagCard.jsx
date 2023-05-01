91d170c144df3b8fa59a24d470caf315c6f2036d
Ilhan Neziric <81989212+ilhanneziric@users.noreply.github.com>
2023-05-01 22:54:16 +0200

    Disabled TagCard bug fix & changed hover to focus

--- rekbooweb/src/Components/TagCard.jsx
+++ rekbooweb/src/Components/TagCard.jsx
@@ -4,20 +4,16 @@ const TagCard = ({addTag, removeTag, name,checked, disabled = false}) => {
   return (
     <>
     {
-      checked?
-      <div className="tagCardContainer" onClick={() => removeTag(name)}>
-            {
-              disabled?
-              <div className="tagCardBody tagCardChecked tagCardBodyDisabled" ><div className="tagCardName tagCardNameChecked tagCardNameDisabled">{name}</div></div>:
-              <div className="tagCardBody tagCardChecked" ><div className="tagCardName tagCardNameChecked">{name}</div></div>
-            }
+      checked ?
+      <div className="tagCardContainer" onClick={() => disabled ? '' :  removeTag(name)}>
+              <div className={disabled ? "tagCardBody tagCardChecked tagCardBodyDisabled" : "tagCardBody tagCardChecked"} >
+                <div className={disabled ? "tagCardName tagCardNameChecked tagCardNameDisabled" : "tagCardName tagCardNameChecked"}>{name}</div>
+              </div>
       </div>:
-      <div className="tagCardContainer" onClick={() => addTag(name)}>
-        {
-          disabled?
-          <div className="tagCardBody tagCardBodyDisabled"><div className="tagCardName tagCardNameDisabled">{name}</div></div>:
-          <div className="tagCardBody"><div className="tagCardName">{name}</div></div>
-        }
+      <div className="tagCardContainer" onClick={() => disabled ? '' : addTag(name)}>
+          <div className={disabled ? "tagCardBody tagCardBodyDisabled" : "tagCardBody"}>
+            <div className={disabled ? "tagCardName tagCardNameDisabled" : "tagCardName"}>{name}</div>
+          </div>
       </div>
     }
     </>
