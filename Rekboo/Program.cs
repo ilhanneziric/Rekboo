2eed595de4c216f666879e460d94f1639d50563f
Amer <amerque@outlookcom>
2023-05-10 18:49:21 +0200

    ssl configured

--- Rekboo/Program.cs
+++ Rekboo/Program.cs
@@ -85,7 +85,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "JWTAuthDemo v1"));
 }
 
-app.UseHttpsRedirection();
 app.UseCors("corsapp");
 
 app.UseAuthentication();
